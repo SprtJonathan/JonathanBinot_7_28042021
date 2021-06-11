93580a5bac4eeebcac1e2fe21bbcf445e4a7158b
Jonathan <jonathan.binot@gmail.com>
2021-06-11 16:53:28 +0200

    Ajout du script sql

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import store from './store'
 import router from './router'
 import moment from 'moment'
 import Toast from "vue-toastification";
+
 // Import the CSS or use your own!
 import "vue-toastification/dist/index.css";
 
